8e7df839f2c57e1679713f0a8cc2b74a9f00d885
Sergio Mellado Martin <alu0101326351@ull.edu.es>
2022-03-29 09:03:51 +0000

    plantilla pruebas

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -5,4 +5,167 @@ import { Song } from '../src/song'
 
 
 
-describe("TEST", () => {})
+describe("SONG TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+
+    it("getArtist() test", () => {
+
+    });
+    
+    it("getGenre() test", () => {
+
+    });
+    
+    it("getIsSingle() test", () => {
+
+    });
+
+    it("getTimesListened() test", () => {
+
+    });
+
+    it("getSeconds() test", () => {
+
+    });    
+    
+    it("getMinutes() test", () => {
+
+    });
+
+});
+
+
+describe("PLAYLIST TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+    
+    it("getGenre() test", () => {
+
+    });
+    
+    it("getHours() test", () => {
+
+    });    
+    
+    it("getMinutes() test", () => {
+
+    });
+
+    it("getSongs() test", () => {
+
+    });
+    
+});
+
+
+
+describe("GROUP TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+    
+    it("getGenres() test", () => {
+
+    });
+    
+    it("getListeners() test", () => {
+
+    });    
+    
+    it("getArtists() test", () => {
+
+    });
+
+    it("getYear() test", () => {
+
+    });
+
+    it("getAlbums() test", () => {
+
+    });
+    
+});
+
+
+
+describe("GENRE TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+    
+    it("getComponents() test", () => {
+
+    });
+    
+    it("getSongs() test", () => {
+
+    });    
+
+    it("getAlbums() test", () => {
+
+    });
+    
+});
+
+
+
+describe("ARTIST TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+
+    it("getGenre() test", () => {
+
+    });
+
+    it("getListeners() test", () => {
+
+    });
+
+    it("getGroups() test", () => {
+
+    });
+
+    it("getAlbums() test", () => {
+
+    });
+
+    it("getSongs() test", () => {
+
+    });
+    
+});
+
+
+
+describe("ARTIST TEST", () => {
+
+    it("getName() test", () => {
+
+    });
+
+    it("getCreator() test", () => {
+
+    });
+
+    it("getYear() test", () => {
+
+    });
+
+    it("getGenres() test", () => {
+
+    });
+
+    it("getSongs() test", () => {
+
+    });
+    
+});
